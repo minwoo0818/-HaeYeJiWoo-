8031032545aff8b393bfc1c2d6830eaebe7c78e0
minwoo0818 <145524060+minwoo0818@users.noreply.github.com>
2025-09-23 14:30:57 +0900

    백엔드 기본 세팅 설정

---
+++ HYJW_Back/src/main/java/com/hyjw_back/HyjwBackApplication.java
@@ -0,0 +1,13 @@
+package com.hyjw_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyjwBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyjwBackApplication.class, args);
+    }
+
+}
---
+++ HYJW_Back/src/test/java/com/hyjw_back/HyjwBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyjw_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyjwBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
