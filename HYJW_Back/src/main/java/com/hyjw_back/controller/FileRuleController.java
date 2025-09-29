1c06c56b84649424cfd801fb9304440c940ce4ee
minwoo0818 <145524060+minwoo0818@users.noreply.github.com>
2025-09-30 01:37:08 +0900

    머지 커밋

--- HYJW_Back/src/main/java/com/hyjw_back/controller/FileRuleController.java
+++ HYJW_Back/src/main/java/com/hyjw_back/controller/FileRuleController.java
@@ -49,9 +49,9 @@ public class FileRuleController { // 클래스 이름을 FileRuleController 대�
     }
 
     @GetMapping("/main")
-    public ResponseEntity<FileRuleDto> getFileRule() {
+    public ResponseEntity<FileSettingsDto> getFileRule() {
         try {
-            FileRuleDto dto = fileRuleService.getCurrentRule();
+            FileSettingsDto dto = fileRuleService.getCurrentRule();
             return ResponseEntity.ok(dto);
         } catch (Exception e) {
             // 상세 오류 로깅 필요
