36b349cd57c1d7e9de5e31ad65b20e7aa939c263
JYB0535 <jnk8528@gmail.com>
2025-09-29 18:28:13 +0900

    관리자 정보 조회 토큰
    ㅇㅇ

--- HYJW_Front/src/api/CommentApi.ts
+++ HYJW_Front/src/api/CommentApi.ts
@@ -4,6 +4,14 @@ import type { AdminComment } from "../type";
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const getCommentsForAdmin = async (): Promise<AdminComment[]> => {
-  const response = await axios.get(`${BASE_URL}/comments/admin`);
+  const token = sessionStorage.getItem("jwt");
+  if (!token) {
+    throw new Error("Authentication token not found.");
+  }
+  const response = await axios.get(`${BASE_URL}/comments/admin`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   return response.data;
 };
