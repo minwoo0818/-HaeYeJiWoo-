bbfaf2bafd663eecaaf9ace38f3f344be2737c00
minwoo0818 <145524060+minwoo0818@users.noreply.github.com>
2025-09-30 14:44:03 +0900

    .

--- HYJW_Front/vite.config.ts
+++ HYJW_Front/vite.config.ts
@@ -4,10 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "localhost",
+    host: "172.20.20.10",
+    port: 80,
+    strictPort: true,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://172.20.20.10:8080",
         rewrite: (path) => path.replace(/^\/api/, ""),
         changeOrigin: true,
       },
